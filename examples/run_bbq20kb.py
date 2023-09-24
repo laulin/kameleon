a438952a6c1569e7d181ba3dbd14f7998abfa7ad
lmoulin <lmoulin@spartan-conseil.fr>
2023-09-24 19:08:11 +0200

    update keyboard example

--- examples/run_bbq20kb.py
+++ examples/run_bbq20kb.py
@@ -1,29 +1,26 @@
 from machine import Pin, I2C
 import time
-import hw
+import hal 
 
-from bbq20kbd import BBQ20Kbd
+tft, _, keyboard, _, lora = hal.setup()
 
 # Show how to use the keyboard
 
-i2c = I2C(0, scl=Pin(hw.SDL, pull=Pin.PULL_UP), sda=Pin(hw.SDA, pull=Pin.PULL_UP), freq=400000)
-kb = BBQ20Kbd(i2c)
-kb.configuration(use_mods=True, report_mods=True)
-print(f"Kb {kb} initiated with {i2c}")
-print(f"version : {kb.version}")
+keyboard.configuration(use_mods=True, report_mods=True)
+print(f"version : {keyboard.version}")
 
 loop = True
 backlight_delta = 1
 backlight_value = 0
 while loop:
-    events = kb.keys
+    events = keyboard.keys
 
     if len(events):
         print(events)
         if (2, " ") in  events:
             loop = False
 
-    x, y = kb.trackpad
+    x, y = keyboard.trackpad
 
     if x !=0 or y !=0 :
         print(f"T({x},{y})")
@@ -35,6 +32,6 @@ while loop:
     if backlight_value <= 0:
         backlight_delta = 1
 
-    kb.backlight = backlight_value
+    keyboard.backlight = backlight_value
 
     time.sleep(0.05)
